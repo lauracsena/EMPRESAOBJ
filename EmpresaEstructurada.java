cedb2ff33fe16c1d37de87434d4dfd4179703046
= <=>
2024-10-08 09:55:15 -0500

    primer java

---
+++ EmpresaEstructurada.java
@@ -0,0 +1,6 @@
+public class EmpresaEstructurada {
+  public static void main(String[] args) throws Exception{
+    String nombre, cargo;
+    double salario, total=0, mayorSalario, menorSalario
+  }  
+}
